ded50d9dc1d2eed67e5134e7ca0622ae67f0d9e4
Alena Farber <alenafarber@gmail.com>
2016-12-06 14:50:44 -0500

    sankey all donegit pushgit pushgit push

--- js/main.js
+++ js/main.js
@@ -151,37 +151,3 @@ function changeNeighborhood(val) {
     console.log(val);
 }*/
 
-
-
-
-
-
-
-/* this is a helper function that wraps svg text */
-/* its used in sankey and neighborhood_rent */
-
-/*Taken from http://bl.ocks.org/mbostock/7555321
- //Wraps SVG text*/
-function wrap(text, width) {
-    var text = d3.select(this[0][0]),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.4,
-        y = text.attr("y"),
-        x = text.attr("x"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
-
-    while (word = words.pop()) {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width) {
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-        };
-    };
-};
