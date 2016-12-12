2af04ea2533a1bf7b0b2530848e152759016440e
mkane12 <Michaelakane@college.harvard.edu>
2016-12-12 13:44:54 -0500

    updated geojson for map

--- js/airbnbNodeMap.js
+++ js/airbnbNodeMap.js
@@ -167,7 +167,6 @@ AirBnBNodeMap.prototype.initVis = function() {
         .attr("transform", function(d) {
             return "translate(" + vis.projection([d.longitude, d.latitude]) + ")";
         })
-        // make node larger and darker on mouseover
         .on("mouseover", function(d) {
             d3.select(this)
                 .attr("r", 5)
@@ -495,7 +494,7 @@ AirBnBNodeMap.prototype.zoomNeigh = function(selNeigh) {
     vis.sel_neigh = selNeigh;
 
     var f = vis.neighborhoodMap.features.filter(function (n, i) {
-        return n.properties.neighbourhood === vis.sel_neigh;
+        return n.properties.NAME === vis.sel_neigh;
     });
 
 
@@ -505,7 +504,9 @@ AirBnBNodeMap.prototype.zoomNeigh = function(selNeigh) {
         return;
     }
 
-    vis.sel_bor = f[0].properties.neighbourhood_group;
+    vis.sel_bor = f[0].properties.CITY.substring(14);
+
+    console.log(vis.sel_bor);
 
     var e = vis.boroughMap.features.filter(function (n, i) {
         return n.properties.borough === vis.sel_bor;
@@ -707,6 +708,8 @@ AirBnBNodeMap.prototype.zoomBor = function(selBor) {
 // function for zooming out
 AirBnBNodeMap.prototype.zoomOut = function() {
     var vis = this;
+
+    document.getElementById('listing-count').innerHTML = (vis.airbnbData.length).toString();
 
     var x, y, k;
     x = vis.width / 2;
